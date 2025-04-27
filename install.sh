a34935874bedf8b7f32a69d7dec266f0f240335b
Ground-Zerro <ksa65s571dozor@gmail.com>
2025-04-27 17:36:24 +1100

    code correction and update

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ for i in $(ip a | sed -n 's/.*nwg\(.*\): <.*UP.*/\1/p'); do
     rem=$(echo $(ndmc -c "show interface Wireguard$i" | sed -n 's/.*remote.*: \(.*\)/\1/p'))
     echo $rem | grep -q '^0\| 0' && continue
     if gnip $i && gnip $i && gnip $i && gnip $i; then
-        echo "START bypass for Wireguard$i."
+        logger "START bypass for Wireguard$i."
         port=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 63000) + 2000}')
         while netstat -nlu | grep -qw $port; do
             port=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 63000) + 2000}')
@@ -29,10 +29,9 @@ for i in $(ip a | sed -n 's/.*nwg\(.*\): <.*UP.*/\1/p'); do
         length=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 65) + 64}')
         nping --udp --count $count --source-port $port --data-length $length --dest-port $(echo $rem | cut -f2 -d' ') ${rem%% *} >/dev/null 2>&1
         ndmc -c "interface Wireguard$i wireguard listen-port $port" >/dev/null 2>&1
-        echo "Bypass for Wireguard$i - COMPLETE."
+        logger "COMPLETE bypass for Wireguard$i."
     fi
 done
-
 exit
 EOF
 chmod +x /opt/etc/ndm/netfilter.d/wgpass.sh
