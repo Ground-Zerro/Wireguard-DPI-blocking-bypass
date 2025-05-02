34d3078b7e46beba4bb4b5e09d596b3ef5876f61
Ground-Zerro <ksa65s571dozor@gmail.com>
2025-05-03 00:37:05 +1100

    update

--- install.sh
+++ install.sh
@@ -5,34 +5,59 @@ opkg install nping
 
 cat << 'EOF' > /opt/etc/ndm/netfilter.d/wgpass.sh
 #!/bin/sh
-PATH=/opt/sbin:/opt/bin:/opt/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+maxtry=5
 
 if ps | grep -v grep | grep "wgpass.sh" | grep -qv "$$"; then
     exit 0
 fi
 
-gnip()
-{
-! ping -I nwg$1 -s0 -qc1 -W1 1.1.1.1 >/dev/null 2>&1
-}
+gnip() { ! ping -I nwg$1 -s0 -qc1 -W1 1.1.1.1 >/dev/null 2>&1; }
 
 for i in $(ip a | sed -n 's/.*nwg\(.*\): <.*UP.*/\1/p'); do
-    rem=$(echo $(ndmc -c "show interface Wireguard$i" | sed -n 's/.*remote.*: \(.*\)/\1/p'))
-    echo $rem | grep -q '^0\| 0' && continue
-    if gnip $i && gnip $i && gnip $i && gnip $i; then
-        logger "START bypass for Wireguard$i."
-        port=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 63000) + 2000}')
-        while netstat -nlu | grep -qw $port; do
+    rem=$(ndmc -c "show interface Wireguard$i" | sed -n 's/.*remote.*: \(.*\)/\1/p')
+    echo "$rem" | grep -q '^0\| 0' && continue
+
+    try=1
+    while [ $try -le $maxtry ]; do
+        if gnip "$i" && gnip "$i" && gnip "$i" && gnip "$i"; then
+            logger "WG bypass: START try $try for Wireguard$i."
+
             port=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 63000) + 2000}')
-        done >/dev/null 2>&1
-        count=$(hexdump -n1 -e '1/1 "%u\n"' /dev/urandom | awk '{print ($1 % 5) + 6}')
-        length=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 65) + 64}')
-        nping --udp --count $count --source-port $port --data-length $length --dest-port $(echo $rem | cut -f2 -d' ') ${rem%% *} >/dev/null 2>&1
-        ndmc -c "interface Wireguard$i wireguard listen-port $port" >/dev/null 2>&1
-        logger "COMPLETE bypass for Wireguard$i."
-    fi
+            while netstat -nlu | grep -qw "$port"; do
+                port=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 63000) + 2000}')
+            done >/dev/null 2>&1
+
+            count=$(hexdump -n1 -e '1/1 "%u\n"' /dev/urandom | awk '{print ($1 % 21) + 30}')
+            rate=$(hexdump -n1 -e '1/1 "%u\n"' /dev/urandom | awk '{print ($1 % 101) + 50}')
+            length=$(hexdump -n2 -e '1/2 "%u\n"' /dev/urandom | awk '{print ($1 % 1537) + 512}')
+
+            rem_ip=$(echo "$rem" | head -n1)
+            rem_port=$(echo "$rem" | tail -n1)
+            nping --udp --count "$count" --rate "$rate" --source-port "$port" --data-length "$length" --dest-port "$rem_port" "$rem_ip" >/dev/null 2>&1
+
+            ndmc -c "interface Wireguard$i wireguard listen-port $port" >/dev/null 2>&1
+
+            sleep 10
+            logger "WG bypass: COMPLETE try $try for Wireguard$i, checking result..."
+
+            if gnip "$i" && gnip "$i" && gnip "$i" && gnip "$i"; then
+                logger "WG bypass: FAIL try $try for Wireguard$i. Retrying..."
+                try=$((try + 1))
+                if [ $try -gt $maxtry ]; then
+                    logger "WG bypass: GIVING UP after $maxtry tries for Wireguard$i."
+                    break
+                fi
+            else
+                logger "WG bypass: SUCCESS for Wireguard$i on try $try."
+                break
+            fi
+        else
+            logger "WG bypass: Wireguard$i already OK."
+            break
+        fi
+    done
 done
-exit
 EOF
 chmod +x /opt/etc/ndm/netfilter.d/wgpass.sh
 
